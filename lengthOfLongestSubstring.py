3651f80d122ffef9eefeb941890ee1d0e1d2c1b0
Anewnoob <474225654@qq.com>
2020-03-06 15:33:54 +0800

    update

--- lengthOfLongestSubstring.py
+++ lengthOfLongestSubstring.py
@@ -1,4 +1,25 @@
+"""
+给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+
+示例 1:
+
+输入: "abcabcbb"
+输出: 3
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+示例 2:
+
+输入: "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+示例 3:
+
+输入: "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+"""
 class Solution:
+    #暴力法
     def lengthOfLongestSubstring(self, s: str) -> int:
         total_lenth = len(s)
         if total_lenth == 0:
@@ -31,7 +52,27 @@ class Solution:
                     break
                 if len(sub_string) > max_len:
                     max_len = len(sub_string)
-
+    #滑动窗口-- 类似蚯蚓前进
+    def lengthOfLongestSubstringv2(self, s: str) -> int:
+        class Solution:
+            def lengthOfLongestSubstring(self, s: str) -> int:
+                if not s:
+                    return 0
+                left = 0
+                lookup = set()
+                n = len(s)
+                max_len = 0
+                cur_len = 0
+                for i in range(n):
+                    cur_len += 1
+                    while s[i] in lookup:
+                        lookup.remove(s[left])
+                        left += 1
+                        cur_len -= 1
+                    if cur_len > max_len:
+                        max_len = cur_len
+                    lookup.add(s[i])
+                return max_len
 
 test = Solution()
 result = test.lengthOfLongestSubstring("cdd")
