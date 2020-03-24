44f8a3c721af84bd6c0c4db92f63491581ef54c7
Anewnoob <474225654@qq.com>
2020-03-24 19:14:15 +0800

    update

--- longestPalindrome.py
+++ longestPalindrome.py
@@ -27,3 +27,40 @@ class Solution:
             else:
                 return False
         return True
+
+
+class Solution2:
+    """
+    动态规划 :画一张 I*J的表格 进行填表  为回文的填true并记录位置 和长度  不是回文False
+    """
+
+    def longestPalindrome(self, s: str) -> str:
+        size = len(s)
+        if size < 2:
+            return s
+
+        dp = [[False for _ in range(size)] for _ in range(size)]
+
+        max_len = 1
+        start = 0
+
+        for i in range(size):
+            dp[i][i] = True
+
+        for j in range(1, size):
+            for i in range(0, j):
+                if s[i] == s[j]:
+                    if j - i < 3:
+                        dp[i][j] = True
+                    else:
+                        dp[i][j] = dp[i + 1][j - 1]
+                else:
+                    dp[i][j] = False
+
+                if dp[i][j]:
+                    cur_len = j - i + 1
+                    if cur_len > max_len:
+                        max_len = cur_len
+                        start = i
+        return s[start:start + max_len]
+
