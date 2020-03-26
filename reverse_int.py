4da2b5464d2d549964bb142168f3eaf097284986
Anewnoob <474225654@qq.com>
2020-03-26 21:50:50 +0800

    update

--- reverse_int.py
+++ reverse_int.py
@@ -9,6 +9,20 @@ class Solution:
 
         return reverse_str if -2147483648 < reverse_str < 2147483647 else 0
 
+    def reverse_better(
+            self,
+            x: int) -> int:
+
+        y, res = abs(x), 0
+        #<< 2进制左移31位
+        boundry = (1 << 31) - 1 if x > 0 else 1 << 31
+        while y != 0:
+            res = res * 10 + y % 10  # %取余数
+            if res > boundry:
+                return 0
+            y //= 10                # 向下取整 除
+        return res if x > 0 else -res
+
 test = Solution()
 result = test.reverse_force(123)
 print(result)
