30d17db6552246e58c1a4f55b5d6c917621323a7
Anewnoob <474225654@qq.com>
2020-04-28 16:05:21 +0800

    Update NumArray.py

--- NumArray.py
+++ NumArray.py
@@ -4,12 +4,21 @@ class NumArray:
         self.nums = nums
 
     def sumRange(self, i: int, j: int) -> int:
-        new_nums = self.nums[i:j+1]
         sumall = 0
-        for value in new_nums:
+        for value in self.nums[i:j+1]:
             sumall += value
         return sumall
 
+###DP
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.dp = [0]
+        for num in nums:
+            self.dp.append(self.dp[-1] + num)
+
+    def sumRange(self, i: int, j: int) -> int:
+        return self.dp[j + 1] - self.dp[i]
 
 
 # Your NumArray object will be instantiated and called as such:
