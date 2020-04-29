202854f9d24f42ecc9f8014cef4a3a2fe394e54e
Anewnoob <474225654@qq.com>
2020-04-29 12:59:41 +0800

    Create isSubsequence.py

---
+++ isSubsequence.py
@@ -0,0 +1,20 @@
+#392. 判断子序列
+#给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+#你可以认为 s 和 t 中仅包含英文小写字母。字符串 t 可能会很长（长度 ~= 500,000），而 s 是个短字符串（长度 <=100）。
+#字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。（例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/is-subsequence
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if not s: return True
+        if not t or len(s) > len(t): return False
+        
+        p,q = 0,0
+        while p < len(s) and q < len(t):
+            if s[p] == t[q]:
+                p += 1
+            q += 1
+        return p == len(s)
