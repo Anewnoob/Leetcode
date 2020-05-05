a4f8a766108efb85f0144a0f2897af936e9b12df
Anewnoob <474225654@qq.com>
2020-05-05 14:11:27 +0800

    Update maxValue.py

--- maxValue.py
+++ maxValue.py
@@ -22,3 +22,16 @@ class Solution:
             for j in range(1,n):
                 dp[i][j] = max(dp[i-1][j],dp[i][j-1]) + grid[i][j]
         return dp[-1][-1]
+
+#better
+class Solution:
+    def maxValue(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        for j in range(1, n): # 初始化第一行
+            grid[0][j] += grid[0][j - 1]
+        for i in range(1, m): # 初始化第一列
+            grid[i][0] += grid[i - 1][0]
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] += max(grid[i][j - 1], grid[i - 1][j])
+        return grid[-1][-1]
