78e87b7ca8a78a4bff53ce2168e821f1a3b53d78
Anewnoob <474225654@qq.com>
2020-05-07 18:41:23 +0800

    Update quickSort.py

--- quickSort.py
+++ quickSort.py
@@ -21,6 +21,17 @@ def quickSort(a,left,right):
     a[i] = temp
     quickSort(a,left,i-1)
     quickSort(a,i+1,right) 
+    
+#冒泡
+def maopaoSort(a):
+    if len(a) <= 1:
+        return a
+    for i in range(len(a)-1):
+        for j in range(len(a)-i-1):
+            if a[j] > a[j+1]:
+                t = a[j]
+                a[j] = a[j+1]
+                a[j+1] = t
 
 a = [4,5,1,2,8,5,1,2,7,3,6,4,8,9,10,2,6,1,8]
 quickSort(a,0,len(a)-1)
