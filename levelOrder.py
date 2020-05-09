1ba9eb6cfdbf6bf486d6ccfe829c59da66f8156e
Anewnoob <474225654@qq.com>
2020-05-09 15:07:45 +0800

    #面试题32 . 从上到下打印二叉树

--- levelOrder.py
+++ levelOrder.py
@@ -19,17 +19,26 @@ class Solution:
                 if node.left: deque.append(node.left)
                 if node.right: deque.append(node.right)
         return res
+    
+#面试题32 - II. 从上到下打印二叉树 II
+#从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root: return []
+        res, deque = [],collections.deque([root])
+        while deque:
+            tmp_deque = []
+            for _ in range(len(deque)):
+                node = deque.popleft()
+                tmp_deque.append(node.val)
+                if node.left: deque.append(node.left)
+                if node.right: deque.append(node.right)
+            res.append(tmp_deque)
+        return res
 
+    
 #面试题32 - III. 从上到下打印二叉树 III
 #请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，其他行以此类推。
-
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
         if not root:
