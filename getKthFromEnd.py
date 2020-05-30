9153f7736987b66831da639749b521cdef25e724
Anewnoob <474225654@qq.com>
2020-05-30 20:05:36 +0800

    面试题22. 链表中倒数第k个节点

---
+++ getKthFromEnd.py
@@ -0,0 +1,21 @@
+#输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+#例如，一个链表有6个节点，从头节点开始，它们的值依次是1、2、3、4、5、6。这个链表的倒数第3个节点是值为4的节点。
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getKthFromEnd(self, head: ListNode, k: int) -> ListNode:
+        if head is None: return None
+        p = ListNode(-1)
+        q = p
+        p.next = head
+        for _ in range(k):
+            p = p.next
+        while p.next is not None:
+            p = p.next
+            q = q.next
+        return q.next
