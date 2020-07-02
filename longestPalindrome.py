87d11e949df715a9ce6866277d70e5a32d671749
Anewnoob <474225654@qq.com>
2020-07-02 11:35:52 +0800

    Update longestPalindrome.py

--- longestPalindrome.py
+++ longestPalindrome.py
@@ -63,4 +63,40 @@ class Solution2:
                         max_len = cur_len
                         start = i
         return s[start:start + max_len]
+    
+    
+        """
+        中心扩散  考虑长度为奇数偶数的两种情况
+        """
+        size = len(s)
+        if size < 2:
+            return s
+
+        # 至少是 1
+        max_len = 1
+        res = s[0]
+
+        def center_spread(s, size, left, right):
+            """
+            left = right 的时候，此时回文中心是一个字符，回文串的长度是奇数
+            right = left + 1 的时候，此时回文中心是一个空隙，回文串的长度是偶数
+            """
+            i,j = left,right
+            while i >= 0 and j < size and s[i] == s[j]:
+                i -= 1
+                j += 1
+            return s[i + 1:j], j - i - 1
+
+        for i in range(size):
+            palindrome_odd, odd_len = center_spread(s, size, i, i)
+            palindrome_even, even_len = center_spread(s, size, i, i + 1)
+
+            # 当前找到的最长回文子串
+            cur_max_sub = palindrome_odd if odd_len >= even_len else palindrome_even
+            if len(cur_max_sub) > max_len:
+                max_len = len(cur_max_sub)
+                res = cur_max_sub
+
+        return res
+
 
