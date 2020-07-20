187f4a8722bb1a07ba35a1a97ebe60f040947f65
Anewnoob <474225654@qq.com>
2020-07-20 22:05:54 +0800

    718. 最长重复子数组 | 1143.最长公共子序列

---
+++ longestCommonSubsequence.py
@@ -0,0 +1,18 @@
+给定两个字符串  text1 和  text2，返回这两个字符串的最长公共子序列的长度。
+
+一个字符串的   子序列   是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。两个字符串的「公共子序列」是这两个字符串所共同拥有的子序列。
+
+class Solution:
+    def maxUncrossedLines(self, A: List[int], B: List[int]) -> int:
+        if not A or not B : return 0
+        m,n = len(A),len(B)
+        dp = [[0]*(n+1) for _ in range(m+1)]
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if A[i-1] == B[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+        return dp[-1][-1]
+
